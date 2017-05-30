543fe66a18657ac3e13562288ec77dd2e427f4d4
Joel Epstein <joel.epstein2@gmail.com>
2017-05-30 12:16:25 -0700

    Basic working about me page

--- app.js
+++ app.js
@@ -1,9 +1,69 @@
 'use strict';
 
-var confirmed = confirmed('Are you ready to rock and roll!?');
+var db = [];
+
+var confirmed = confirm('Are you ready to become a rockstar!?');
 
 if(confirmed === true){
   console.log('The user is ready!');
 } else {
   console.log('The user said they were not ready... but were gonna start anyway.');
+}
+
+var user = prompt('What is your name? Mine is Joel.');
+console.log('The user\'s name is: ' + user);
+console.log(db);
+alert('Hello ' + user + '... I\'m going to ask you a few questions.');
+
+var answer1 = prompt(user + ' this is a yes or no question, so please answer with a Y or N. Do you think I was in the Air Force?');
+db[0] = {'Do you think I was in the Air Force?' : answer1};
+
+if (answer1.toUpperCase() === 'Y') {
+  alert('Yes! You are correct!!! I was on active duty for 6.5 years!');
+} else {
+  alert('No... you are wrong.');
+}
+
+var answer2 = prompt(user + ' please answer with a Yes or No. Do you believe I am 27 years old?');
+db[1] = {'Do you believe I am 27 years old?': answer2};
+
+if (answer2.toLowerCase() === 'yes') {
+  alert('You are correct!');
+} else {
+  alert('Naw.');
+}
+
+var answer3 = prompt('Do you think I have a cat? This is a yes or no question');
+db[2] = {'Do you think I have a cat?': answer3};
+
+if (answer3.toLowerCase() === 'yes') {
+  alert('You are amazing and correct!');
+} else {
+  alert('No, sorry you are wrong bro.');
+}
+
+var answer4 = prompt('Do I have a little brother? This is a y or n question...');
+db[3] = {'Do I have a little brother?': answer4};
+
+if (answer4.toLowerCase() === 'y') {
+  alert('You are correct!');
+} else {
+  alert('Nope.');
+}
+
+var answer5 = prompt('Do you think I currently have a GTO? yes or no please ' + user + '.');
+db[4] = {'Do you think I currently have a GTO?': answer5};
+
+if (answer5.toLowerCase() === 'yes') {
+  alert('No... Sadly I sold it.');
+} else {
+  alert('You are amazing and correct! ');
+}
+
+var confirmed = confirm('Thanks for answering my questions ' + user + ' Confirm to move on to my About me!');
+
+if(confirmed === true){
+  console.log('The user is ready!');
+} else {
+  console.log('The user said they were not ready... Too bad I run this.');
 }
