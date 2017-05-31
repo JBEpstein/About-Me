4ca0834740093435d1f611a948316d02fdc64cef
Joel Epstein <joel.epstein2@gmail.com>
2017-05-30 19:07:38 -0700

    Added some more console.log to correct/incorrect

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 var db = [];
 
+var correct = 0
+var incorrect = 0
 var confirmed = confirm('Are you ready to become a rockstar!?');
 
 if(confirmed === true){
@@ -20,8 +22,12 @@ db[0] = {'Do you think I was in the Air Force? Correct answer: Y...' : answer1};
 
 if (answer1.toUpperCase() === 'Y') {
   alert('Yes! You are correct!!! I was on active duty for 6.5 years!');
+  correct ++;
+  console.log('Correct= ' + correct);
 } else {
   alert('No... you are wrong.');
+  incorrect ++;
+  console.log('incorrect');
 }
 
 var answer2 = prompt(user + ' please answer with a Yes or No. Do you believe I am 27 years old?');
@@ -29,8 +35,12 @@ db[1] = {'Do you believe I am 27 years old? Correct answer: Yes...': answer2};
 
 if (answer2.toLowerCase() === 'yes') {
   alert('You are correct!');
+  correct ++;
+  console.log('correct');
 } else {
   alert('Naw. Sorry you are wrong');
+  incorrect ++;
+  console.log('incorrect');
 }
 
 var answer3 = prompt('Do you think I have a cat? This is a yes or no question, please answer with a yes or no.');
@@ -38,8 +48,12 @@ db[2] = {'Do you think I have a cat? Correct answer: yes...': answer3};
 
 if (answer3.toLowerCase() === 'yes') {
   alert('You are amazing and correct!');
+  correct ++;
+  console.log('correct');
 } else {
   alert('No, sorry you are wrong bro.');
+  incorrect ++;
+  console.log('incorrect');
 }
 
 var answer4 = prompt('Do I have a little brother? This is a y or n question, so please answer with a y or n.');
@@ -47,8 +61,12 @@ db[3] = {'Do I have a little brother? Correct answer: y...': answer4};
 
 if (answer4.toLowerCase() === 'y') {
   alert('You are correct!');
+  correct ++;
+  console.log('correct');
 } else {
   alert('Nope. You are wrong...');
+  incorrect ++;
+  console.console.log(incorrect);
 }
 
 var answer5 = prompt('Do you think I currently have a GTO? yes or no please ' + user + '.');
@@ -56,8 +74,12 @@ db[4] = {'Do you think I currently have a GTO? Correct answer: no...': answer5};
 
 if (answer5.toLowerCase() === 'yes') {
   alert('No... Sadly I sold it.');
+  correct ++;
+  console.log('correct');
 } else {
   alert('You are amazing and correct! ');
+  incorrect ++;
+  console.log('incorrect');
 }
 
 var confirmed = confirm('Thanks for answering my questions ' + user + ' Confirm to move on to my About me!');
