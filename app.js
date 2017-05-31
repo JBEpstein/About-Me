76e3359e4d36cc243bb72f8d6761e71ee61afd66
Joel Epstein <joel.epstein2@gmail.com>
2017-05-30 19:38:41 -0700

    Fixed correct/incorrect var's

--- app.js
+++ app.js
@@ -22,12 +22,12 @@ db[0] = {'Do you think I was in the Air Force? Correct answer: Y...' : answer1};
 
 if (answer1.toUpperCase() === 'Y') {
   alert('Yes! You are correct!!! I was on active duty for 6.5 years!');
-  correct ++;
-  console.log('Correct= ' + correct);
+  correct +=1;
+  console.log('correct= ' + correct);
 } else {
   alert('No... you are wrong.');
-  incorrect ++;
-  console.log('incorrect');
+  incorrect +=1;
+  console.log('incorrect= ' + incorrect);
 }
 
 var answer2 = prompt(user + ' please answer with a Yes or No. Do you believe I am 27 years old?');
@@ -35,11 +35,11 @@ db[1] = {'Do you believe I am 27 years old? Correct answer: Yes...': answer2};
 
 if (answer2.toLowerCase() === 'yes') {
   alert('You are correct!');
-  correct ++;
+  correct +=1;
   console.log('correct');
 } else {
   alert('Naw. Sorry you are wrong');
-  incorrect ++;
+  incorrect +=1;
   console.log('incorrect');
 }
 
@@ -48,11 +48,11 @@ db[2] = {'Do you think I have a cat? Correct answer: yes...': answer3};
 
 if (answer3.toLowerCase() === 'yes') {
   alert('You are amazing and correct!');
-  correct ++;
+  correct +=1;
   console.log('correct');
 } else {
   alert('No, sorry you are wrong bro.');
-  incorrect ++;
+  incorrect +=1;
   console.log('incorrect');
 }
 
@@ -61,12 +61,12 @@ db[3] = {'Do I have a little brother? Correct answer: y...': answer4};
 
 if (answer4.toLowerCase() === 'y') {
   alert('You are correct!');
-  correct ++;
-  console.log('correct');
+  correct +=1;
+  console.log('correct= ' + correct);
 } else {
   alert('Nope. You are wrong...');
-  incorrect ++;
-  console.console.log(incorrect);
+  incorrect +=1;
+  console.console.log('incorrect= ' + incorrect);
 }
 
 var answer5 = prompt('Do you think I currently have a GTO? yes or no please ' + user + '.');
@@ -74,15 +74,15 @@ db[4] = {'Do you think I currently have a GTO? Correct answer: no...': answer5};
 
 if (answer5.toLowerCase() === 'yes') {
   alert('No... Sadly I sold it.');
-  correct ++;
-  console.log('correct');
+  correct +=1;
+  console.log('correct= ' + correct);
 } else {
   alert('You are amazing and correct! ');
-  incorrect ++;
-  console.log('incorrect');
+  incorrect +=1;
+  console.log('incorrect= ' + incorrect);
 }
 
-var confirmed = confirm('Thanks for answering my questions ' + user + ' Confirm to move on to my About me!');
+var confirmed = confirm('Thanks for answering my questions ' + user + '!' + ' You got ' + correct + ' correct!');
 
 if(confirmed === true){
   console.log('The user is ready!');
